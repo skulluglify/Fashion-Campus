e697904d6e817ea841485f85d84890e0985a36a4
Ardian Arvon <vonarvon@gmail.com>
2022-11-26 23:02:33 +0700

    minor update#4 get products

--- api/route/products.py
+++ api/route/products.py
@@ -51,6 +51,7 @@ def get_products():
         # return jsonify({ "message": "error, category not valid" }), 400
         # pass
     
+    min_price, max_price = 0, 10000000
     # ikut FE
     try:
         body_price = "biar lolos if"
